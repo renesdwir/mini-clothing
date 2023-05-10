506377d74e8b75166204f4344485260d752ac6bf
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-10 22:11:02 +0700

    category item

---
+++ src/components/category-item/category-item.component.tsx
@@ -0,0 +1,25 @@
+import { CategoryTypes } from "../../types";
+import "./category-item.styles.scss";
+
+interface CategoryItemProps {
+  category: CategoryTypes;
+}
+
+const CategoryItem = ({ category }: CategoryItemProps) => {
+  const { title, imageUrl } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+export default CategoryItem;
