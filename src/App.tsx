716f682705e9e6b605aa17fe7239eafeafde14e6
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-13 22:56:26 +0700

    add nested route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Homepage from "./routes/homepage/homepage.component";
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Homepage />} />
+      </Route>
     </Routes>
   );
 };
