f9b9fd955cad918cb9760c696718fcb1f555a9d9
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-14 22:27:07 +0700

    create navigation component

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,9 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import CrwnLogo from "../../assets/crown.svg";
+import "./navigation.styles.scss";
 
 const Navigation = () => {
   return (
     <>
-      <div>Navigation</div>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img src={CrwnLogo} alt="Logo" className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-item" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
       <Outlet />
     </>
   );
