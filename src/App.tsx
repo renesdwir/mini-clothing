a8e9409fcfc97c62c8ef5925bbe12dd740dd2c8e
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-15 22:30:14 +0700

    create root for signin & shop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import Homepage from "./routes/homepage/homepage.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
+
+const Shop = () => {
+  return <h1>Shop Page</h1>;
+};
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Homepage />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="signin" element={<SignIn />} />
       </Route>
     </Routes>
   );
