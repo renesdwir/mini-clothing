553c0ecfd2524bf1b5cc87ff7e79c55d7d18c169
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-17 22:45:20 +0700

    setup firebase

---
+++ src/utils/firebase/firebase.utils.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwOESYX9agQWG275SyPO4PXQYaoOLA-Aw",
+  authDomain: "mini-clothing.firebaseapp.com",
+  projectId: "mini-clothing",
+  storageBucket: "mini-clothing.appspot.com",
+  messagingSenderId: "644920877289",
+  appId: "1:644920877289:web:9ba3001c2b7e7a51e55078",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
