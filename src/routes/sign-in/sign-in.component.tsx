c786f55186f892bff4e796b66e4fd175d3658d09
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-18 16:39:27 +0700

    test signin

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -1,4 +1,15 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
-  return <h1>SignIn Page</h1>;
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
+  return (
+    <>
+      <h1>sign in</h1>
+      <button onClick={logGoogleUser}>sign in with google popup</button>
+    </>
+  );
 };
 export default SignIn;
