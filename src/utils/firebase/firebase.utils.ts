d8ba1fd86f6b5131b6eb005e287dc6f9a56f054d
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-19 22:43:48 +0700

    creating user when user didnt exist

--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -7,6 +7,7 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import { UserAuthTypes } from "../../types";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,10 +27,25 @@ const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
   prompt: "select_account",
 });
+
 const db = getFirestore();
-export const createUserDocumentFromAuth = async (userAuth: { uid: string }) => {
+export const createUserDocumentFromAuth = async (userAuth: UserAuthTypes) => {
   const userDocRef = doc(db, "users", userAuth.uid);
   const userSnapshot = await getDoc(userDocRef);
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user: ", error);
+    }
+  }
+  return userDocRef;
 };
 
 export const auth = getAuth();
