da97cb2f0b5e77fa86bd98bba8a03edd6d03050c
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-20 23:10:34 +0700

    set provider signin redirect

--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -50,3 +50,5 @@ export const createUserDocumentFromAuth = async (userAuth: UserAuthTypes) => {
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
