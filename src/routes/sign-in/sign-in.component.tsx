39672d24d53223a246fcf70d7d20e1aa5f228975
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-20 23:10:45 +0700

    signin redirect

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -2,6 +2,7 @@ import { UserAuthTypes } from "../../types";
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
@@ -9,10 +10,16 @@ const SignIn = () => {
     const { user }: { user: UserAuthTypes } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+  const logGoogleUserRedirect = async () => {
+    const { user }: { user: UserAuthTypes } = await signInWithGoogleRedirect();
+  };
   return (
     <>
       <h1>sign in</h1>
       <button onClick={logGoogleUser}>sign in with google popup</button>
+      <button onClick={logGoogleUserRedirect}>
+        sign in with google redirect
+      </button>
     </>
   );
 };
