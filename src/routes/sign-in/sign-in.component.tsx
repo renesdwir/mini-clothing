fae832c5eefbb6339076bee71e1b37efaf3bafdd
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-21 21:05:14 +0700

    get data from signin redirect

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -1,11 +1,20 @@
 import { UserAuthTypes } from "../../types";
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
   signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 
 const SignIn = () => {
+  useEffect(async () => {
+    const response = await getRedirectResult(auth);
+    if (response) {
+      const userDocRef = await createUserDocumentFromAuth(response.user);
+    }
+  }, []);
   const logGoogleUser = async () => {
     const { user }: { user: UserAuthTypes } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
