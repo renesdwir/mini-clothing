9600b362f8759c02ffc5b9857eacee38dec7189f
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-21 21:21:35 +0700

    form for sign up

--- src/components/sign-up-form/sign-up-form.component.tsx
+++ src/components/sign-up-form/sign-up-form.component.tsx
@@ -1,7 +1,20 @@
 const SignUpForm = () => {
   return (
     <div>
-      <h1>signupform</h1>
+      <h1>Sign up with your email and password</h1>
+      <form>
+        <label>Display Name</label>
+        <input type="text" name="" id="" required />
+
+        <label>Email</label>
+        <input type="text" name="" id="" required />
+
+        <label>Password</label>
+        <input type="text" name="" id="" required />
+
+        <label>Confirm password</label>
+        <input type="text" name="" id="" required />
+      </form>
     </div>
   );
 };
