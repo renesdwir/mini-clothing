1cbd470bb78fa224318d6ec49d439256e2929daf
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-22 21:15:15 +0700

    refactor signup form

--- src/components/sign-up-form/sign-up-form.component.tsx
+++ src/components/sign-up-form/sign-up-form.component.tsx
@@ -2,19 +2,22 @@ const SignUpForm = () => {
   return (
     <div>
       <h1>Sign up with your email and password</h1>
-      <form>
+      <form onSubmit={() => {}}>
         <label>Display Name</label>
-        <input type="text" name="" id="" required />
+        <input type="text" name="Display-Name" required />
 
         <label>Email</label>
-        <input type="text" name="" id="" required />
+        <input type="email" name="Email" required />
 
         <label>Password</label>
-        <input type="text" name="" id="" required />
+        <input type="password" name="Password" required />
 
         <label>Confirm password</label>
-        <input type="text" name="" id="" required />
+        <input type="password" name="Confirm-Password" required />
+        <button type="submit">Sign Up</button>
       </form>
     </div>
   );
 };
+
+export default SignUpForm;
