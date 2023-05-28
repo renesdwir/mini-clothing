e012c68bc219a58223cb664bcd402a3368713a5d
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-05-28 22:30:31 +0700

    add button attributes

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,5 +1,21 @@
-const Button = ({ children }) => {
-  return <button className="button-container">{children}</button>;
+type ButtonType = "google" | "inverted";
+
+const BUTTON_TYPE_CLASSES: Record<ButtonType, string> = {
+  google: "google-sign-in",
+  inverted: "inverted",
+};
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  buttonType: ButtonType;
+}
+const Button = ({ children, buttonType, ...otherProps }: ButtonProps) => {
+  return (
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
