e8318e58e6fc2d2e9886b6069729b5fb8a0283fb
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-06-13 20:19:34 +0700

    feat : Cart Icon

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -1,5 +1,5 @@
 import "./cart-icon.styles.scss";
-import ShoppingIcon from "../../assets/shopping-bag.svg";
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 const CartIcon = () => {
   return (
