eb3587fb12a4b32e78697d37053c106cb366e454
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-06-13 10:01:14 +0700

    add svgr config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        // svgr options
+      },
+    }),
+  ],
 });
