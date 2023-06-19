0e5b1508e91af2e7f918ee4ffc01aa38e8c8f929
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-06-19 21:17:31 +0700

    refactor : helper function

--- src/components/checkout-item/checkout-item.component.tsx
+++ src/components/checkout-item/checkout-item.component.tsx
@@ -7,23 +7,27 @@ const CheckoutItem = ({ item }: { item: CartItemTypes }) => {
   const { id, imageUrl, name, price, quantity } = item;
   const { changeQuantity, removeCartItem } = useContext(CartContext);
   const removeHandler = () => removeCartItem(id);
+  const decrementHandler = () => changeQuantity(id, "dec");
+  const incrementHandler = () => changeQuantity(id, "inc");
   return (
     <div className="checkout-item-container">
       <div className="image-container">
         <img src={imageUrl} alt={name} />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity} </span>
+      <span className="quantity">
+        <div className="arrow" onClick={decrementHandler}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={incrementHandler}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={removeHandler}>
         &#10005;
       </div>
-      {/* <h2>{name}</h2>
-      <span>{quantity}</span>
-      <br />
-      <span onClick={() => changeQuantity(id, "dec")}>Decrement</span>
-      <br />
-      <span onClick={() => changeQuantity(id, "inc")}>Increment</span> */}
     </div>
   );
 };
