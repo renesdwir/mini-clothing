3a9f556cc86f8201bcfbbccf050d7df2e3d6599e
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-06-25 19:40:09 +0700

    add title

--- src/routes/category/category.component.tsx
+++ src/routes/category/category.component.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import "./category.styles.scss";
-import { useContext, useEffect, useState } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 import { CategoriesContext } from "../../context/categories.context";
 import { CategoryMapTypes } from "../../types";
 import ProductCard from "../../components/product-card/product-card.component";
@@ -14,11 +14,14 @@ const Category = () => {
     if (category) setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
   return (
-    <div className="categoryy-container">
-      {products?.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+    <Fragment>
+      <h2 className="category-title">{category?.toUpperCase()}</h2>
+      <div className="category-container">
+        {products?.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </Fragment>
   );
 };
 export default Category;
