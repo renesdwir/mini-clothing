3cb7b2406eb64f066faecee02d88278d9bd5de61
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-06-25 19:39:22 +0700

    renaming directory item

--- src/components/directory-item/directory-item.component.tsx
+++ src/components/directory-item/directory-item.component.tsx
@@ -1,25 +1,25 @@
 import { CategoryTypes } from "../../types";
-import "./category-item.styles.scss";
+import "./directory-item.styles.scss";
 
-interface CategoryItemProps {
+interface DirectoryItemProps {
   category: CategoryTypes;
 }
 
-const CategoryItem = ({ category }: CategoryItemProps) => {
+const DirectoryItem = ({ category }: DirectoryItemProps) => {
   const { title, imageUrl } = category;
   return (
-    <div className="category-container">
+    <div className="directory-item-container">
       <div
         className="background-image"
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className="category-body-container">
+      <div className="directory-item-body-container">
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
     </div>
   );
 };
-export default CategoryItem;
+export default DirectoryItem;
