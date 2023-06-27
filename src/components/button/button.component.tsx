591817b98166fb28f0bfda0f163351922164e4c2
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-06-27 21:30:04 +0700

    refactor : button component with styled components

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,25 +1,36 @@
-import "./button.styles.scss";
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+} from "./button.styles";
 
-type ButtonType = "google" | "inverted";
+type ButtonType = "base" | "google" | "inverted";
 
-const BUTTON_TYPE_CLASSES: Record<ButtonType, string> = {
-  google: "google-sign-in",
+export const BUTTON_TYPE_CLASSES: Record<ButtonType, string> = {
+  base: "base",
+  google: "google",
   inverted: "inverted",
 };
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   buttonType?: ButtonType;
 }
-const Button = ({ children, buttonType, ...otherProps }: ButtonProps) => {
-  return (
-    <button
-      className={`button-container ${
-        buttonType ? BUTTON_TYPE_CLASSES[buttonType] : ""
-      }`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
+const getButton = (buttonType: string = BUTTON_TYPE_CLASSES.base) => {
+  const buttonComponents = {
+    [BUTTON_TYPE_CLASSES.base]: BaseButton,
+    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+  };
+
+  return buttonComponents[buttonType as ButtonType];
+};
+
+export const Button = ({
+  children,
+  buttonType,
+  ...otherProps
+}: ButtonProps) => {
+  const CustomButton = getButton(buttonType);
+  return <CustomButton {...otherProps}>{children}</CustomButton>;
 };
 
 export default Button;
