41a98c51c793b4326ec4c1ba754168b632289248
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-06-28 22:55:44 +0700

    refactor styles with styledcomponents

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,9 +1,13 @@
 import Button from "../button/button.component";
 import { CartItem } from "../cart-item/cart-item.component";
-import "./cart-dropdown.styles.scss";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../context/cart.context";
+import {
+  CartDropdownContainer,
+  CartItems,
+  EmptyMessage,
+} from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
   const { cartItems, setIsCartOpen } = useContext(CartContext);
@@ -14,18 +18,18 @@ const CartDropdown = () => {
     navigate("/checkout");
   };
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
+    <CartDropdownContainer>
+      <CartItems>
         {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
         ) : (
-          <span>Your Cart Is Empty</span>
+          <EmptyMessage>Your Cart Is Empty</EmptyMessage>
         )}
-      </div>
+      </CartItems>
       <Button onClick={goToCheckout} buttonType="base">
         GO TO CHECKOUT
       </Button>
-    </div>
+    </CartDropdownContainer>
   );
 };
 
