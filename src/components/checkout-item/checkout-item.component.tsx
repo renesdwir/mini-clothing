9d36ba2925726e2e162ff02786874edd44706f19
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-07-02 20:14:16 +0700

    refactor : checkout item with SC

--- src/components/checkout-item/checkout-item.component.tsx
+++ src/components/checkout-item/checkout-item.component.tsx
@@ -1,7 +1,16 @@
 import { CartItemTypes } from "../../types";
-import "./checkout-item.styles.scss";
 import { CartContext } from "../../context/cart.context";
 import { useContext } from "react";
+import {
+  CheckoutItemContainer,
+  ImageContainer,
+  RemoveButton,
+  StyledArrow,
+  StyledName,
+  StyledPrice,
+  StyledQuantity,
+  StyledValue,
+} from "./checkout-item.styles";
 
 const CheckoutItem = ({ item }: { item: CartItemTypes }) => {
   const { id, imageUrl, name, price, quantity } = item;
@@ -10,25 +19,19 @@ const CheckoutItem = ({ item }: { item: CartItemTypes }) => {
   const decrementHandler = () => changeQuantity(id, "dec");
   const incrementHandler = () => changeQuantity(id, "inc");
   return (
-    <div className="checkout-item-container">
-      <div className="image-container">
+    <CheckoutItemContainer>
+      <ImageContainer>
         <img src={imageUrl} alt={name} />
-      </div>
-      <span className="name">{name}</span>
-      <span className="quantity">
-        <div className="arrow" onClick={decrementHandler}>
-          &#10094;
-        </div>
-        <span className="value">{quantity}</span>
-        <div className="arrow" onClick={incrementHandler}>
-          &#10095;
-        </div>
-      </span>
-      <span className="price">{price}</span>
-      <div className="remove-button" onClick={removeHandler}>
-        &#10005;
-      </div>
-    </div>
+      </ImageContainer>
+      <StyledName>{name}</StyledName>
+      <StyledQuantity>
+        <StyledArrow onClick={decrementHandler}>&#10094;</StyledArrow>
+        <StyledValue>{quantity}</StyledValue>
+        <StyledArrow onClick={incrementHandler}>&#10095;</StyledArrow>
+      </StyledQuantity>
+      <StyledPrice>{price}</StyledPrice>
+      <RemoveButton onClick={removeHandler}>&#10005;</RemoveButton>
+    </CheckoutItemContainer>
   );
 };
 
