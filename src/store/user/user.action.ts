67c3a620d7b90f101dbd29f47b49c202e9b41bf7
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-07-21 22:38:53 +0700

    export setcurrentuser

--- src/store/user/user.action.ts
+++ src/store/user/user.action.ts
@@ -1,5 +1,5 @@
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { USER_ACTION_TYPES } from "./user.types";
 
-const setCurrentUser = (user: any) =>
+export const setCurrentUser = (user: any) =>
   createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
