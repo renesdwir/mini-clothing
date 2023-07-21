eb157fde2a47c17b66ea1a242cff5087cf9686b4
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-07-21 22:37:32 +0700

    remove provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 
 import App from "./App.tsx";
-import { UserProvider } from "./context/user.context";
+// import { UserProvider } from "./context/user.context";
 import { CartProvider } from "./context/cart.context.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
@@ -14,13 +14,13 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <UserProvider>
-          {/* <CategoriesProvider> */}
-          <CartProvider>
-            <App />
-          </CartProvider>
-          {/* </CategoriesProvider> */}
-        </UserProvider>
+        {/* <UserProvider> */}
+        {/* <CategoriesProvider> */}
+        <CartProvider>
+          <App />
+        </CartProvider>
+        {/* </CategoriesProvider> */}
+        {/* </UserProvider> */}
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
