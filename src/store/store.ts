da45720e6780cecf9d81c2ba2f55c996f25da6ac
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-07-22 23:06:18 +0700

    add categories reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,9 +2,11 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { combineReducers } from "redux";
 import { userReducer } from "./user/user.reducer";
+import { categoriesReducer } from "./categories/category.reducer";
 
 export const rootReducer = combineReducers({
   user: userReducer,
+  categories: categoriesReducer,
 });
 
 export const store = createStore(rootReducer, applyMiddleware(logger));
