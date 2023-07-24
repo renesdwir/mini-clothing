e83b8a08d87b8bf75f1b0b406ed98b84d9b24b0f
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-07-24 21:14:33 +0700

    set categories map

--- src/routes/shop/shop.component.tsx
+++ src/routes/shop/shop.component.tsx
@@ -1,16 +1,28 @@
+import { useEffect } from "react";
 import { CategoriesProvider } from "../../context/categories.context";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { Routes, Route } from "react-router-dom";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { setCategoriesMap } from "../../store/categories/category.action";
+import { useDispatch } from "react-redux";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      dispatch(setCategoriesMap(categoryMap));
+    };
+    getCategoriesMap();
+  }, []);
   return (
-    <CategoriesProvider>
-      <Routes>
-        <Route index element={<CategoriesPreview />} />
-        <Route path=":category" element={<Category />} />
-      </Routes>
-    </CategoriesProvider>
+    // <CategoriesProvider>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
+    // </CategoriesProvider>
   );
 };
 export default Shop;
