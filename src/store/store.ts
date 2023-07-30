1afa01b51d110e1ee7aaba2a7b65cc86ee00569e
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-07-30 20:39:33 +0700

    add saga middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,19 +2,26 @@ import { createStore, applyMiddleware, Middleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
-import { logger } from "./middleware/logger";
-import thunk from "redux-thunk";
+// import { logger } from "./middleware/logger";
+import logger from "redux-logger";
+// import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./root-saga";
 
 const persistConfig = {
   key: "root",
   storage,
   whitelist: ["cart"],
 };
+
+const sagaMiddleware = createSagaMiddleware();
+
 let middleWares: Middleware[] = [];
 
 if (process.env.NODE_ENV !== "production") {
   middleWares.push(logger);
-  middleWares.push(thunk);
+  middleWares.push(sagaMiddleware);
+  // middleWares.push(thunk);
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -22,4 +29,6 @@ export const store = createStore(
   persistedReducer,
   applyMiddleware(...middleWares)
 );
+
+sagaMiddleware.run(rootSaga);
 export const persistor = persistStore(store);
