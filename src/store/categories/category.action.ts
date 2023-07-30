65654bc919fb4640d45071d0357c0f24f884bfe2
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-07-30 20:39:53 +0700

    create category saga

--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -1,5 +1,3 @@
-import { Dispatch } from "redux";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
@@ -12,13 +10,3 @@ export const fetchCategoriesSuccess = (categoriesArray: any) =>
   );
 export const fetchCategoriesFailed = (error: any) =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
-
-export const fetchCategoriesAsync = () => async (dispatch: Dispatch) => {
-  dispatch(fetchCategoriesStart());
-  try {
-    const categoriesArray = await getCategoriesAndDocuments("categories");
-    dispatch(fetchCategoriesSuccess(categoriesArray));
-  } catch (error) {
-    dispatch(fetchCategoriesFailed(error));
-  }
-};
