26c1dd70daf16199b12800637b667cb3649ce646
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-08-02 22:17:10 +0700

    dispatch fetch category start

--- src/routes/shop/shop.component.tsx
+++ src/routes/shop/shop.component.tsx
@@ -2,15 +2,13 @@ import { useEffect } from "react";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { Routes, Route } from "react-router-dom";
-import { fetchCategoriesAsync } from "../../store/categories/category.action";
+import { fetchCategoriesStart } from "../../store/categories/category.action";
 import { useDispatch } from "react-redux";
-import { ThunkDispatch } from "redux-thunk";
-import { AnyAction } from "redux";
 
 const Shop = () => {
-  const dispatch: ThunkDispatch<any, any, AnyAction> = useDispatch();
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchCategoriesAsync());
+    dispatch(fetchCategoriesStart());
   }, []);
   return (
     <Routes>
